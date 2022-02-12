ecaae6ea752974451a997f49bbbf017a61c0cacf
Pavel Gatin <imnotcreative@yandex.ru>
2022-02-13 03:46:36 +0700

    Мелкие правки по работе секции 'workouts'

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,8 @@
+function centerScrollableElement(elem) {
+  let leftOffset = Math.round((elem.scrollWidth - elem.offsetWidth) / 2);
+  elem.scrollLeft = leftOffset;
+}
+
 let imageGallery = document.getElementById("image-gallery");
 let imageGalleryCard = document.querySelector(".image-gallery__card");
 const imageGalleryLeftControl = document.getElementById("image-gallery-left-control");
@@ -5,11 +10,22 @@ const imageGalleryRightControl = document.getElementById("image-gallery-right-co
 
 imageGalleryLeftControl.addEventListener("click", () => {
   let step = imageGalleryCard.offsetWidth + parseInt(window.getComputedStyle(imageGallery).columnGap);
-  imageGallery.scrollLeft -= step;
-
+  let scrollLeft = imageGallery.scrollLeft - step;
+  imageGallery.scrollLeft = scrollLeft;
+  let maxScrollLeft = imageGallery.scrollWidth - imageGallery.offsetWidth;
+  imageGalleryLeftControl.style.opacity = scrollLeft > 0 ? "1" : "0.3";
+  imageGalleryRightControl.style.opacity = scrollLeft < maxScrollLeft ? "1" : "0.3";
 })
 
 imageGalleryRightControl.addEventListener("click", () => {
   let step = imageGalleryCard.offsetWidth + parseInt(window.getComputedStyle(imageGallery).columnGap);
-  imageGallery.scrollLeft += step;
+  let scrollLeft = imageGallery.scrollLeft + step;
+  imageGallery.scrollLeft = scrollLeft;
+  let maxScrollLeft = imageGallery.scrollWidth - imageGallery.offsetWidth;
+  imageGalleryLeftControl.style.opacity = scrollLeft > 0 ? "1" : "0.3";
+  imageGalleryRightControl.style.opacity = scrollLeft < maxScrollLeft ? "1" : "0.3";
 })
+
+window.onload = function() {
+  centerScrollableElement(imageGallery);
+}
