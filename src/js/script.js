62d40bab1250e3ad680bcae333fddf85c1040162
Pavel Gatin <imnotcreative@yandex.ru>
2022-02-13 16:05:02 +0700

    Заверстать секцию 'community'

--- src/js/script.js
+++ src/js/script.js
@@ -1,31 +1,69 @@
-function centerScrollableElement(elem) {
-  let leftOffset = Math.round((elem.scrollWidth - elem.offsetWidth) / 2);
-  elem.scrollLeft = leftOffset;
-}
-
 let imageGallery = document.getElementById("image-gallery");
 let imageGalleryCard = document.querySelector(".image-gallery__card");
-const imageGalleryLeftControl = document.getElementById("image-gallery-left-control");
-const imageGalleryRightControl = document.getElementById("image-gallery-right-control");
+const imageGalleryControls = {
+  left: document.getElementById("image-gallery-left-control"),
+  right: document.getElementById("image-gallery-right-control")
+};
 
-imageGalleryLeftControl.addEventListener("click", () => {
-  let step = imageGalleryCard.offsetWidth + parseInt(window.getComputedStyle(imageGallery).columnGap);
-  let scrollLeft = imageGallery.scrollLeft - step;
-  imageGallery.scrollLeft = scrollLeft;
-  let maxScrollLeft = imageGallery.scrollWidth - imageGallery.offsetWidth;
-  imageGalleryLeftControl.style.opacity = scrollLeft > 0 ? "1" : "0.3";
-  imageGalleryRightControl.style.opacity = scrollLeft < maxScrollLeft ? "1" : "0.3";
-})
+let testimonialGallery = document.getElementById("testimonial-gallery");
+let testimonialGalleryCard = document.querySelector(".image-gallery__testimonial-card");
+const testimonialGalleryControls = {
+  left: document.getElementById("testimonial-gallery-left-control"),
+  right: document.getElementById("testimonial-gallery-right-control")
+};
 
-imageGalleryRightControl.addEventListener("click", () => {
-  let step = imageGalleryCard.offsetWidth + parseInt(window.getComputedStyle(imageGallery).columnGap);
-  let scrollLeft = imageGallery.scrollLeft + step;
-  imageGallery.scrollLeft = scrollLeft;
-  let maxScrollLeft = imageGallery.scrollWidth - imageGallery.offsetWidth;
-  imageGalleryLeftControl.style.opacity = scrollLeft > 0 ? "1" : "0.3";
-  imageGalleryRightControl.style.opacity = scrollLeft < maxScrollLeft ? "1" : "0.3";
-})
+function centerScrollableElement(scrollableElem, elemControls) {
+  let leftOffset = Math.round((scrollableElem.scrollWidth - scrollableElem.offsetWidth) / 2);
+  updateScrollableElemControls(scrollableElem,
+                               elemControls,
+                               leftOffset);
+  scrollableElem.scrollLeft = leftOffset;
+}
+
+function updateScrollableElemControls(scrollableElem, elemControls, scrollLeft) {
+  let maxScrollLeft = scrollableElem.scrollWidth - scrollableElem.offsetWidth;
+  elemControls.left.style.opacity = scrollLeft > 0 ? "1" : "0.3";
+  elemControls.right.style.opacity = scrollLeft < maxScrollLeft ? "1" : "0.3";
+}
+
+function handleControlClick(scrollableElem, scrollableElemCard, elemControls, orientation) {
+  let step = scrollableElemCard.offsetWidth + parseInt(window.getComputedStyle(scrollableElem).columnGap);
+  let newScrollLeft = orientation === "left"  ? scrollableElem.scrollLeft - step :
+                      orientation === "right" ? scrollableElem.scrollLeft + step :
+                                                scrollableElem.scrollLeft;
+  updateScrollableElemControls(scrollableElem, elemControls, newScrollLeft);
+  scrollableElem.scrollLeft = newScrollLeft;
+}
+
+imageGalleryControls.left.addEventListener("click", () => {
+  handleControlClick(imageGallery,
+                     imageGalleryCard,
+                     imageGalleryControls,
+                     "left");
+});
+
+imageGalleryControls.right.addEventListener("click", () => {
+  handleControlClick(imageGallery,
+                     imageGalleryCard,
+                     imageGalleryControls,
+                     "right");
+});
+
+testimonialGalleryControls.left.addEventListener("click", () => {
+  handleControlClick(testimonialGallery,
+                     testimonialGalleryCard,
+                     testimonialGalleryControls,
+                     "left");
+});
+
+testimonialGalleryControls.right.addEventListener("click", () => {
+  handleControlClick(testimonialGallery,
+                     testimonialGalleryCard,
+                     testimonialGalleryControls,
+                     "right");
+});
 
 window.onload = function() {
-  centerScrollableElement(imageGallery);
+  centerScrollableElement(imageGallery, imageGalleryControls);
+  centerScrollableElement(testimonialGallery, testimonialGalleryControls);
 }
